b11bc09be4640e2a013760bcf51b1aad2bb21667
Garrett Reinhart <garretrein1984@gmail.com>
2023-12-27 15:47:47 -0800

    first

---
+++ src/components/navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Typography } from "antd";
+import "./style.css";
+
+const { Title } = Typography;
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-content">
+        <div className="nav-home"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
